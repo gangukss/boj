886c9fc0821e300fabe231edfdf86544e03810f7
kan <kwk7328@gmail.com>
2021-11-18 23:45:40 +0900

    pass 11726 11727

---
+++ codeplus_set/400/11727.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int ans[1001];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+	ans[0] = 1;
+	ans[1] = 1;
+	for (int i = 2; i <= n; ++i)
+		ans[i] = (ans[i - 1] + ans[i - 2] * 2) % 10007;
+	cout << ans[n];
+}
