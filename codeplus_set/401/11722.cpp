256e8798c15182212f6bb080a5b45311d9cdb5d3
kankim <rkddnr7328@naver.com>
2021-11-29 20:03:21 +0900

    pass 11722

---
+++ codeplus_set/401/11722.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int A[1000];
+int dp[1000];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, ans = 0;
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> A[i];
+		dp[i] = 1;
+		for (int j = i - 1; j >= 0; --j)
+			if (A[j] > A[i])
+				dp[i] = max(dp[j] + 1, dp[i]);
+		ans = max(ans, dp[i]);
+	}
+	cout << ans;
+}
