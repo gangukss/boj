df7ecad6183213154b5d4d51bd417b352dc8e2f8
kankim <rkddnr7328@naver.com>
2021-12-09 13:56:07 +0900

    pass 10973

---
+++ codeplus_set/520/10973.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int input[10000];
+
+bool myCmp(int i, int j) {
+	return i > j;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+		cin >> input[i];
+	for (int i = n - 1; i >= 0; --i) {
+		if (i == 0) {
+			cout << -1;
+			return 0;
+		}
+		if (input[i - 1] < input[i])
+			continue ;
+		for (int j = n - 1; j >= 0; --j) {
+			if (input[i - 1] > input[j]) {
+				int tmp = input[i - 1];
+				input[i - 1] = input[j];
+				input[j] = tmp;
+				break ;
+			}
+		}
+		sort(input + i, input + n, myCmp);
+		break ;
+	}
+	for (int i = 0; i < n; ++i)
+		cout << input[i] << " ";
+}
